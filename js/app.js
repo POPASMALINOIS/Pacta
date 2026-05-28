2e7de815da7131c3ebd722dcc0068afd9428ceb6
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2026-05-28 17:51:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-import "./auth.js";
+const loginTab = document.getElementById("loginTab");
+const registerTab = document.getElementById("registerTab");
+const nameField = document.getElementById("nameField");
 
-import {
-  listenGroups
-} from "./groups.js";
+window.pactaAuthMode = "login";
 
-setTimeout(() => {
-  listenGroups();
-}, 1500);
+loginTab.addEventListener("click", () => {
+  window.pactaAuthMode = "login";
+
+  loginTab.classList.add("active");
+  registerTab.classList.remove("active");
+  nameField.classList.add("hidden");
+});
+
+registerTab.addEventListener("click", () => {
+  window.pactaAuthMode = "register";
+
+  registerTab.classList.add("active");
+  loginTab.classList.remove("active");
+  nameField.classList.remove("hidden");
+});
+
+import("./auth.js").catch((error) => {
+  console.error("Error cargando auth.js:", error);
+});
