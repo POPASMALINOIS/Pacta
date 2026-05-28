6ede039ec67a78ad90608daf1f8553cde932cf16
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2026-05-28 17:55:17 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -8,8 +8,6 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-
  const firebaseConfig = {
   apiKey: "AIzaSyAgVAk3oMXyLQNRJVUZjhbZg5PZyQab1A8",
   authDomain: "pacta-844ce.firebaseapp.com",
@@ -18,17 +16,6 @@ const firebaseConfig = {
   messagingSenderId: "163475092846",
   appId: "1:163475092846:web:e2ed70d46f788e8a64fd7f",
   measurementId: "G-95NMVY7PCL"
-};
-
-  authDomain: "TU_AUTH_DOMAIN",
-
-  projectId: "TU_PROJECT_ID",
-
-  storageBucket: "TU_STORAGE_BUCKET",
-
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-
-  appId: "TU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
