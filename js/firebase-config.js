c834dde91fcb84eb730c98aeb7e88a9e2ba7ff62
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2026-05-28 17:07:49 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "TU_API_KEY",
+
+  authDomain: "TU_AUTH_DOMAIN",
+
+  projectId: "TU_PROJECT_ID",
+
+  storageBucket: "TU_STORAGE_BUCKET",
+
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+
+  appId: "TU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
