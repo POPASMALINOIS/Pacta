e9e87f1c36af0f75839c7d140bfdf17158c89d52
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2026-05-28 17:43:09 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,7 +10,15 @@ import {
 
 const firebaseConfig = {
 
-  apiKey: "TU_API_KEY",
+ const firebaseConfig = {
+  apiKey: "AIzaSyAgVAk3oMXyLQNRJVUZjhbZg5PZyQab1A8",
+  authDomain: "pacta-844ce.firebaseapp.com",
+  projectId: "pacta-844ce",
+  storageBucket: "pacta-844ce.firebasestorage.app",
+  messagingSenderId: "163475092846",
+  appId: "1:163475092846:web:e2ed70d46f788e8a64fd7f",
+  measurementId: "G-95NMVY7PCL"
+};
 
   authDomain: "TU_AUTH_DOMAIN",
 
